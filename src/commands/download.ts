4acc2d9d6eaab1c5725d26562703fb1363ed5c15
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-05-08 21:49:45 +0900

    loop per page

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -3,14 +3,21 @@ import axios, { AxiosResponse } from 'axios';
 import { CommandOptions } from '../interfaces/command-options';
 
 const itemsPerPage = 100;
+const maxPageNumber = 100;
 
 export const download = async (options: CommandOptions): Promise<void> => {
   const authenticatedUser = await loadAuthenticatedUserInfo(options);
   // 公開している記事数
   const itemCount = authenticatedUser.data.items_count;
-  let page = 1;
-  const authenticatedUserItems = await loadAuthenticatedUserItems(options, page);
-  console.log('test');
+  let page = 0;
+  while (page * itemsPerPage < itemCount) {
+    page = page + 1;
+    if (page > maxPageNumber) {
+      break;
+    }
+    const authenticatedUserItems = await loadAuthenticatedUserItems(options, page);
+    console.log(authenticatedUserItems.data.length);
+  }
 };
 
 async function loadAuthenticatedUserInfo(options: CommandOptions): Promise<AxiosResponse<any, any>> {
